18ddc155e59c8f6c94722e3cd1f536b18d5d8785
Alex Engelhardt <alex@hall-of-heroes.com>
2024-04-08 13:36:20 +1000

    adding title graphic

--- src/scenes/Main.ts
+++ src/scenes/Main.ts
@@ -1,17 +1,30 @@
 import { Scene } from "phaser";
 
 export class Main extends Scene {
+  background: Phaser.GameObjects.Image | undefined;
+  title: Phaser.GameObjects.Image | undefined;
+  titleTween: Phaser.Tweens.Tween | undefined;
+
   constructor() {
     super("Main");
   }
 
   preload() {
     this.load.image("background", "assets/bg.png");
+    this.load.image("title", "assets/title.png");
   }
 
   create() {
     const scene = this;
 
-    scene.add.image(0, 0, "background").setOrigin(0);
+    scene.background = scene.add.image(0, 0, "background").setOrigin(0);
+    scene.title = scene.add.image(512, 384, "title").setOrigin(0.5, 0.5);
+
+    scene.titleTween = scene.tweens.add({
+      targets: scene.title,
+      scale: { value: 1.3, duration: 700, ease: "Back.easeInOut" },
+      yoyo: true,
+      repeat: -1,
+    });
   }
 }
